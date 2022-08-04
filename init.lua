c16d7e42d17958da021893d13ea056d264ecee13
Aaron von Awesome <gitlab@vonawesome.email>
2022-08-04 11:03:12 -0400

    Change colorscheme from opt to g

--- init.lua
+++ init.lua
@@ -40,7 +40,7 @@ vim.opt.wildignore = vim.opt.wildignore + 'tags'
 -- Colors/Color Schemes
 vim.opt.termguicolors = true
 vim.g.codedark_conservative = 1
-vim.opt.colorscheme = 'codedark'
+vim.g.colorscheme = 'codedark'
 
 -- Set indent character for indent-blankline.nvim plugin
 vim.g.indent_blankline_char = '⦙'
