6e08affcaa4fb4b8ef1ca84d615a0dc4a0c6155c
Aaron von Awesome <gitlab@vonawesome.email>
2022-08-09 17:11:28 -0400

    Refactor from ' to " and from api.nvim_set_keymap to keymap.set

--- init.lua
+++ init.lua
@@ -49,17 +49,17 @@ autocmd({"BufWritePre"}, {
 -- -----------------------------------------------------------
 
 -- Set Leader Key
-vim.g.mapleader = ' '
-vim.api.nvim_set_keymap( 'n', '<space>', '<nop>', {noremap = true})
+vim.g.mapleader = " "
+vim.keymap.set( "n", "<space>", "<nop>", {noremap = true})
 
 -- Syntax Highlighting
-vim.opt.syntax = 'on'
+vim.opt.syntax = "on"
 
 -- Search Path
-vim.opt.path = vim.opt.path + '**'
+vim.opt.path = vim.opt.path + "**"
 
 -- Ignore tags file in searches like vimgrep
-vim.opt.wildignore = vim.opt.wildignore + 'tags'
+vim.opt.wildignore = vim.opt.wildignore + "tags"
 
 -- Colors/Color Schemes
 vim.opt.termguicolors = true
@@ -67,7 +67,7 @@ vim.g.codedark_conservative = 1
 vim.cmd [[colorscheme codedark]]
 
 -- Set indent character for indent-blankline.nvim plugin
-vim.g.indent_blankline_char = '⦙'
+vim.g.indent_blankline_char = "⦙"
 
 -- File Browsing Window properties
 vim.g.netrw_browse_split = 0
@@ -78,7 +78,7 @@ vim.g.netrw_winsize = 25
 vim.opt.hidden = true
 
 -- Sign Column Settings
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Line Number Settings
 vim.opt.number.relativenumber = true
@@ -101,12 +101,12 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 --Show substitutions incrementally
-vim.opt.inccommand = 'nosplit'
+vim.opt.inccommand = "nosplit"
 
 -- File History Settings
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = '~/.vim/undodir'
+vim.opt.undodir = "~/.vim/undodir"
 vim.opt.undofile = true
 
 -- Scrolling Options
@@ -121,99 +121,99 @@ vim.opt.splitbelow = true
 -- ----------------------------------------------------------
 --                  Custom Key Bindings
 -- ----------------------------------------------------------
--- vim.api.nvim_set_keymap('<mode>', '<keys>', '<actions>', {<options>})
+-- vim.keymap.set("<mode>", "<keys>", "<actions>", {<options>})
 
 -- Quick reload of init.lua
-vim.api.nvim_set_keymap('n', '<leader><CR>', ':luafile ~/.config/nvim/init.lua<CR>', {noremap = true})
+vim.keymap.set("n", "<leader><CR>", ":luafile ~/.config/nvim/init.lua<CR>", {noremap = true})
 
 -- Navigate between windows easier
-vim.api.nvim_set_keymap('n', '<leader>h', ':wincmd h<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>j', ':wincmd j<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>k', ':wincmd k<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>l', ':wincmd l<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader><tab>', '<c-^>', {noremap = true})
+vim.keymap.set("n", "<leader>h", ":wincmd h<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>j", ":wincmd j<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>k", ":wincmd k<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>l", ":wincmd l<CR>", {noremap = true})
+vim.keymap.set("n", "<leader><tab>", "<c-^>", {noremap = true})
 
 -- Move lines of blocks of lines at once
-vim.api.nvim_set_keymap('v', 'J', ':m \'>+1<CR>gv=gv', {noremap = true})
-vim.api.nvim_set_keymap('v', 'K', ':m \'<-2<CR>gv=gv', {noremap = true})
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {noremap = true})
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {noremap = true})
 
 -- Shortcut for :noh
-vim.api.nvim_set_keymap('n', '<leader>nh', ':noh<CR>', {noremap = true})
+vim.keymap.set("n", "<leader>nh", ":noh<CR>", {noremap = true})
 
 -- Open tree view for current file directory
-vim.api.nvim_set_keymap('n', '<leader>tv', ':wincmd v<bar> Ex <bar> vertical resize 35<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>th', ':wincmd s<bar> Ex <bar> resize 25<CR>', {noremap = true})
+vim.keymap.set("n", "<leader>tv", ":wincmd v<bar> Ex <bar> vertical resize 35<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>th", ":wincmd s<bar> Ex <bar> resize 25<CR>", {noremap = true})
 
 -- Return to Normal mode whilst in Insert mode using the Terminal
-vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap = true})
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {noremap = true})
 
 -- Still need to be able to send <Esc> whilst using Terminal
-vim.api.nvim_set_keymap('t', '<A-[>', '<Esc>', {noremap = true})
+vim.keymap.set("t", "<A-[>", "<Esc>", {noremap = true})
 
 -- I keep typing Q
-vim.api.nvim_set_keymap('n', 'Q', '<nop>', {noremap = true})
+vim.keymap.set("n", "Q", "<nop>", {noremap = true})
 
 -- Generate Graphviz custom key binding (TODO: How do I detect a dot file, and
 -- only allow this to run against a dot file?)
-vim.api.nvim_set_keymap('n', '<leader>ggv', ':w<CR>:!dot -T png % -o %:r.png<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>vgv', ':!display %:r.png &<CR>', {noremap = true})
+vim.keymap.set("n", "<leader>ggv", ":w<CR>:!dot -T png % -o %:r.png<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>vgv", ":!display %:r.png &<CR>", {noremap = true})
 
 -- Copy & Paste to/from system clipboard
-vim.api.nvim_set_keymap('n', '<leader>y', '"+y', {noremap = true})
-vim.api.nvim_set_keymap('v', '<leader>y', '"+y', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>Y', 'gg"+yG', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>p', '"+p', {noremap = true})
-vim.api.nvim_set_keymap('v', '<leader>p', '"+p', {noremap = true})
+vim.keymap.set("n", "<leader>y", "\"+y", {noremap = true})
+vim.keymap.set("v", "<leader>y", "\"+y", {noremap = true})
+vim.keymap.set("n", "<leader>Y", "gg\"+yG", {noremap = true})
+vim.keymap.set("n", "<leader>p", "\"+p", {noremap = true})
+vim.keymap.set("v", "<leader>p", "\"+p", {noremap = true})
 
 -- Copy relative path
-vim.api.nvim_set_keymap('n', '<leader>cf', ':let @+=expand("%")<CR>', {noremap = true})
+vim.keymap.set("n", "<leader>cf", ":let @+=expand(\"%\")<CR>", {noremap = true})
 
 -- Copy absolute path
-vim.api.nvim_set_keymap('n', '<leader>cF', ':let @+=expand("%:p")<CR>', {noremap = true})
+vim.keymap.set("n", "<leader>cF", ":let @+=expand(\"%:p\")<CR>", {noremap = true})
 
 -- Window Controls
-vim.api.nvim_set_keymap('n', '<A-Up>', ':lua vim.cmd[[vert resize +5]]<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<A-Down>', ':lua vim.cmd[[vert resize -5]]<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-Up>', ':lua vim.cmd[[resize +5]]<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-Down>', ':lua vim.cmd[[resize -5]]<CR>', {noremap = true})
+vim.keymap.set("n", "<A-Up>", ":lua vim.cmd[[vert resize +5]]<CR>", {noremap = true})
+vim.keymap.set("n", "<A-Down>", ":lua vim.cmd[[vert resize -5]]<CR>", {noremap = true})
+vim.keymap.set("n", "<C-Up>", ":lua vim.cmd[[resize +5]]<CR>", {noremap = true})
+vim.keymap.set("n", "<C-Down>", ":lua vim.cmd[[resize -5]]<CR>", {noremap = true})
 
 -- Spell Check
-vim.api.nvim_set_keymap('n', '<leader>ss', ':lua vim.opt.spell = true<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>sns', ':lua vim.opt.spell = false<CR>', {noremap = true})
+vim.keymap.set("n", "<leader>ss", ":lua vim.opt.spell = true<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>sns", ":lua vim.opt.spell = false<CR>", {noremap = true})
 
 -- Find next spelling mistake
-vim.api.nvim_set_keymap('n', '<leader>sl', ']s', {noremap = true})
+vim.keymap.set("n", "<leader>sl", "]s", {noremap = true})
 -- Find previous spelling mistake
-vim.api.nvim_set_keymap('n', '<leader>sh', '[s', {noremap = true})
+vim.keymap.set("n", "<leader>sh", "[s", {noremap = true})
 -- z= - Replace misspelled word
 -- zg - Good word: Add the word under the cursor to the dictionary
 -- zw - Woops! Undo and remove the word from the dictionary
 
 -- Word Wrap
 vim.opt.wrap = false
-vim.api.nvim_set_keymap('n', '<leader>snw', ':lua vim.opt.wrap = false<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>sw', ':lua vim.opt.wrap = true<CR>', {noremap = true})
+vim.keymap.set("n", "<leader>snw", ":lua vim.opt.wrap = false<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>sw", ":lua vim.opt.wrap = true<CR>", {noremap = true})
 
 -- Quickfix List Navigation
-vim.api.nvim_set_keymap('n', '<C-j>', ':cnext<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-k>', ':cprev<CR>', {noremap = true})
+vim.keymap.set("n", "<C-j>", ":cnext<CR>", {noremap = true})
+vim.keymap.set("n", "<C-k>", ":cprev<CR>", {noremap = true})
 
 -- -----------------------------------------------------------
 --                        Status Bar
 -- -----------------------------------------------------------
 
 -- Left Side
-vim.opt.statusline = ''
-vim.opt.statusline = vim.opt.statusline + '\\ %M'
-vim.opt.statusline = vim.opt.statusline + '\\ %y'
-vim.opt.statusline = vim.opt.statusline + '\\ %r'
-vim.opt.statusline = vim.opt.statusline + '\\ %F'
+vim.opt.statusline = ""
+vim.opt.statusline = vim.opt.statusline + " %M"
+vim.opt.statusline = vim.opt.statusline + " %y"
+vim.opt.statusline = vim.opt.statusline + " %r"
+vim.opt.statusline = vim.opt.statusline + " %F"
 
 -- Right side
-vim.opt.statusline = vim.opt.statusline + '%='
-vim.opt.statusline = vim.opt.statusline + '\\ %c:%l/%L'
-vim.opt.statusline = vim.opt.statusline + '\\ %p%%'
-vim.opt.statusline = vim.opt.statusline + '\\ [%n]'
+vim.opt.statusline = vim.opt.statusline + "%="
+vim.opt.statusline = vim.opt.statusline + " %c:%l/%L"
+vim.opt.statusline = vim.opt.statusline + " %p%%"
+vim.opt.statusline = vim.opt.statusline + " [%n]"
 
 -- -----------------------------------------------------------
 --                       Markdown Composer Options
@@ -226,7 +226,7 @@ vim.g.markdown_composer_autostart = 0
 -- -----------------------------------------------------------
 -- Find files using Telescope
 
-vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', {noremap = true})
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>", {noremap = true})
