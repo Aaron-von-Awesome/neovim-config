a6553a6eedc8eea47ac7422bc7c86ec4e6946285
Aaron von Awesome <gitlab@vonawesome.email>
2022-08-12 18:21:10 -0400

    Add cmp-nvim-lua plugin

--- lua/config/nvim_cmp.lua
+++ lua/config/nvim_cmp.lua
@@ -53,10 +53,12 @@ cmp.setup({
       end
     end, { "i", "s" })
   }),
-  sources = cmp.config.sources(
-    { { name = "nvim_lsp" }, { name = 'luasnip' } },
-    { { name = "buffer" } }
-  ),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "nvim_lua" },
+    { name = "buffer" }
+  }),
   formatting = {
     format = lspkind.cmp_format({
       mode = "symbol_text",
