fe1e062373d2135100de076ec1aa8899d84e719e
Aaron von Awesome <gitlab@vonawesome.email>
2022-08-22 14:58:28 -0400

    Fix function calls

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -3,6 +3,6 @@
 -- ======================================================================
 
 vim.keymap.set("n", "<leader>ff", ":lua require('telescope.builtin').find_files({hidden = true})<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>fg", ":lua require('telescope.builtin').live_grep<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>fb", ":lua require('telescope.builtin').buffers<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>fh", ":lua require('telescope.builtin').help_tags<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>fg", ":lua require('telescope.builtin').live_grep()<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>fb", ":lua require('telescope.builtin').buffers()<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>fh", ":lua require('telescope.builtin').help_tags()<CR>", { noremap = true })
