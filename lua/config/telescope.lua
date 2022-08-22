afd112d1bd3cf2a4f8310b10cdd562aeb4fe6741
Aaron von Awesome <gitlab@vonawesome.email>
2022-08-22 12:45:28 -0400

    Switch telescope keymaps over to lua functions, and search for hidden file extensions

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -2,7 +2,7 @@
 --                    Telescope Plugin Configuration
 -- ======================================================================
 
-vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>ff", ":lua require('telescope.builtin').find_files({hidden = true})<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>fg", ":lua require('telescope.builtin').live_grep<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>fb", ":lua require('telescope.builtin').buffers<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>fh", ":lua require('telescope.builtin').help_tags<CR>", { noremap = true })
