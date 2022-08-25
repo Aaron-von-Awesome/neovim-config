6515bd4b3509c6eaa8eed7c82726e08d72160ce6
Aaron von Awesome <gitlab@vonawesome.email>
2022-08-25 16:12:15 -0400

    Fix diagnostic kepmaps

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -6,8 +6,8 @@
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
-vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_next, opts)
-vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, opts)
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
