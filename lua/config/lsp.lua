18fd9edf779fda1890a369e189ea0afa3b8d8532
Aaron von Awesome <codeberg@vonawesome.email>
2025-09-28 00:18:39 -0400

    Add "yaml.ansible" file type to Ansible Language Server detection

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -45,7 +45,7 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protoc
 vim.lsp.config("ansiblels", {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { "yaml", "yml", "ansible" },
+  filetypes = { "yaml", "yml", "ansible", "yaml.ansible" },
   single_file_support = false
 })
 vim.lsp.enable("ansiblels")
