d318b0b2969bcf04ad1f3bf7545d98a77b3e03d9
Aaron von Awesome <codeberg@vonawesome.email>
2025-12-03 21:16:19 -0500

    Remap cmp.mapping.scroll_docs keymaps

--- lua/config/plugins/nvim_cmp.lua
+++ lua/config/plugins/nvim_cmp.lua
@@ -23,8 +23,8 @@ cmp.setup({
     entries = { name = "custom", selection_order = "near_cursor" }
   },
   mapping = cmp.mapping.preset.insert({
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-h>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-s>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
